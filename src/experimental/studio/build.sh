6f27fa6cc69e69206f6dab63dd2cadc2349261f4
Matija Kecman <matijak@google.com>
2026-04-13 13:42:15 +0100

    junk

--- src/experimental/studio/build.sh
+++ src/experimental/studio/build.sh
@@ -44,11 +44,9 @@ if [[ "$do_configure" == true ]]; then
     echo "Configuring MuJoCo Studio (${build_type})..."
     CMAKE_CONFIG_ARGS=(
         "-B build"
-        "-G Ninja"
         "-DCMAKE_BUILD_TYPE=${build_type}"
         "-DUSE_STATIC_LIBCXX=OFF"
         "-DBUILD_SHARED_LIB=OFF"
-        "-DMUJOCO_USE_FILAMENT=OFF"
         "-DMUJOCO_USE_FILAMENT=OFF"
         "-DMUJOCO_USE_FILAMENT_VULKAN=OFF"
         "-DMUJOCO_BUILD_EXAMPLES=OFF"
@@ -59,14 +57,10 @@ if [[ "$do_configure" == true ]]; then
         "-DMUJOCO_BUILD_STUDIO=ON"
     )
 
-    if [[ "${build_type}" == "Debug" ]]; then
-        CMAKE_CONFIG_ARGS+=("-DCMAKE_CXX_FLAGS=\"-O0 -g3\"")
-        CMAKE_CONFIG_ARGS+=("-DCMAKE_C_FLAGS=\"-O0 -g3\"")
-    fi
-
     # Add user-defined CMAKE_ARGS at the end so they override other settings.
     if [[ -n "${CMAKE_ARGS}" ]]; then
-        CMAKE_CONFIG_ARGS+=("${CMAKE_ARGS}")
+        read -a cmake_args_arr <<<"$CMAKE_ARGS"
+        CMAKE_CONFIG_ARGS+=("${cmake_args_arr[@]}")
     fi
 
     cmake ${CMAKE_CONFIG_ARGS[@]}
@@ -77,14 +71,14 @@ fi
 # Build MuJoCo Studio
 if [[ "$do_build" == true ]]; then
     echo "Building MuJoCo Studio..."
-    cmake --build build --target mujoco_studio --parallel
+    cmake --build build --config=${build_type} --target mujoco_studio --parallel
     echo "Building MuJoCo Studio... DONE"
 
     # Print the command to run the built MuJoCo Studio from the right directory.
     echo "Use the following command to run mujoco_studio"
     echo ""
     if [[ "${OS_NAME}" == "windows" ]]; then
-        echo " cd $(git rev-parse --show-toplevel)/build/bin && ./mujoco_studio.exe "
+        echo " cd $(git rev-parse --show-toplevel)/build/bin && ./${build_type}/mujoco_studio.exe "
     else
         echo " cd $(git rev-parse --show-toplevel)/build/bin && ./mujoco_studio "
     fi
